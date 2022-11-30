13fa9f71d8f1589ac56038d366d62cbe161e3d90
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-11-29 18:11:37 -0800

    modified:   src/components/selectMenus/ticket-menu.js

--- src/components/selectMenus/ticket-menu.js
+++ src/components/selectMenus/ticket-menu.js
@@ -24,6 +24,7 @@ module.exports = {
                     
                     guild.channels.create(`ticket-${jsonData.commissionCount}`, {
                         parent: '1046130044944273438',
+                        type: 'GUILD_TEXT',
                         permissionOverwrites: [
                             {
                               id: interaction.member.id,
