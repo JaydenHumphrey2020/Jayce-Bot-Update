ede7c7abab21ea2f458e15aca2b1d26fb0ea5d52
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-11-29 17:11:11 -0800

    modified:   src/commands/support/ticket.js

--- src/commands/support/ticket.js
+++ src/commands/support/ticket.js
@@ -24,7 +24,7 @@ module.exports = {
                 })
             );
 
-            await interaction.reply({
+            await channel.send({
                 components: [new ActionRowBuilder().addComponents(ticketMenu)],
             });
 
