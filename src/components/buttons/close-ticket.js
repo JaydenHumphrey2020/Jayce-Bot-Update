6646ca05cb6aa5718bd331c8573025216b0a5710
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-12-02 03:21:36 -0800

    modified:   src/components/buttons/close-ticket.js

--- src/components/buttons/close-ticket.js
+++ src/components/buttons/close-ticket.js
@@ -6,41 +6,10 @@ module.exports = {
     },
     async execute(interaction, client) {
 
-        // get channel closing
-        const channel = interaction.channel
-
-        channel.messages.fetch().then(async (messages) => {
-
-            const output = messages.array().reverse().map(m => `${new Date(m.createdAt).toLocaleString('en-US')} - ${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n');
-            let response;
-            try {
-                response = await sourcebin.create([
-                    {
-                        name: ' ',
-                        content: output,
-                        languageId: 'text',
-                    },
-                ], {
-                    title: `Chat transcript for ${channel.name}`,
-                    description: ' ',
-                });
-            }
-            catch(e) {
-                return interaction.channel.send('An error occurred, please try again!');
-            }
-            interaction.reply('the transcript is complete. Please click the link below to view the transcript', response.url);
-
-        })
-
-
-
-
-
-
         await interaction.reply({
-            content: 'Closing Ticket in 60 seconds',
+            content: 'Closing Ticket in 10 seconds',
         }).then(
-            setTimeout(() => interaction.channel.delete(), 60000)         
+            setTimeout(() => interaction.channel.delete(), 10000)         
         )
     }
 }
