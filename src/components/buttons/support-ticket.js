3613bcb1c3679237a90d00213ad50f56e5b5b42d
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-12-01 21:41:08 -0800

    modified:   src/components/buttons/support-ticket.js

--- src/components/buttons/support-ticket.js
+++ src/components/buttons/support-ticket.js
@@ -45,7 +45,7 @@ module.exports = {
                     embeds: [SupportEmbed],
                     components: [new ActionRowBuilder().addComponents(closeButton)]
                 });
-                channel.send(`<@1017539524294881310>`).then()
+                channel.send(`<@&1017539524294881310>`).then()
                 
             }
 
