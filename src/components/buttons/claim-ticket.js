265a6451d01e268ed2dc5f8707543d17dac77313
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-12-01 22:06:45 -0800

    modified:   src/components/buttons/claim-ticket.js

--- src/components/buttons/claim-ticket.js
+++ src/components/buttons/claim-ticket.js
@@ -5,6 +5,6 @@ module.exports = {
         name: 'claim-ticket',
     },
     async execute(interaction, client) {
-        console.log(interaction.message.components)
+        console.log(interaction.message.components.ActionRow.components)
     }
 }
