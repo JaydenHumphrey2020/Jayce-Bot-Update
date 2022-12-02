b5fb66d330175c5eca95c3436e32a8655317b0f1
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-12-02 02:59:46 -0800

    modified:   src/components/buttons/claim-ticket.js

--- src/components/buttons/claim-ticket.js
+++ src/components/buttons/claim-ticket.js
@@ -24,7 +24,9 @@ module.exports = {
             .setStyle(ButtonStyle.Primary)
             .setDisabled(true)
 
-        if(interaction.member.roles.cache.has(1017539524294881310)) {
+        let supportRole = message.guild.roles.cache.get("1017539524294881310");
+
+        if(interaction.member.roles.cache.has(supportRole)) {
             interaction.message.edit({
                 embeds: [SupportEmbed],
                 components: [new ActionRowBuilder().addComponents(closeButton,claimButton)]
