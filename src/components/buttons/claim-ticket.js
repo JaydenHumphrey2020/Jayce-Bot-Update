2878c202126a23ca4e67ef4ce813dda491f2ba22
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-12-02 02:32:00 -0800

    modified:   src/components/buttons/claim-ticket.js

--- src/components/buttons/claim-ticket.js
+++ src/components/buttons/claim-ticket.js
@@ -1,10 +1,33 @@
 
+const { EmbedBuilder, PermissionFlagsBits, ChannelType, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+const fs = require('fs');
 
 module.exports = {
     data: {
         name: 'claim-ticket',
     },
     async execute(interaction, client) {
-        interaction.setDisabled(true)
+
+        const SupportEmbed = new EmbedBuilder()
+            .setTitle(`Support Request!`)
+            .setDescription(`A Support Team Member will be with you shortly. In the mean time please provide the reason you need support.`)
+            .setColor(0x18e1ee)
+        
+        const closeButton = new ButtonBuilder()
+            .setCustomId('close-ticket')
+            .setLabel('Close')
+            .setStyle(ButtonStyle.Primary);
+
+        const claimButton = new ButtonBuilder()
+            .setCustomId('claim-ticket')
+            .setLabel('Claim')
+            .setStyle(ButtonStyle.Primary)
+            .setDisabled(true)
+
+
+        interaction.message.edit({
+            embeds: [SupportEmbed],
+            components: [new ActionRowBuilder().addComponents(closeButton,claimButton)]
+        })
     }
 }
