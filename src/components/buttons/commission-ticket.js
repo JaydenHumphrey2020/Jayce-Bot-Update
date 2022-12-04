708c422d6b2ebb0f1a633cb1b7aa9ffd7fe0d84b
JaydenHumphrey2020 <70485397+JaydenHumphrey2020@users.noreply.github.com>
2022-12-03 19:38:25 -0800

    modified:   src/components/buttons/commission-ticket.js 	modified:   src/components/buttons/support-ticket.js

--- src/components/buttons/commission-ticket.js
+++ src/components/buttons/commission-ticket.js
@@ -41,7 +41,7 @@ module.exports = {
             permissionOverwrites: [
 
                 {id: interaction.user.id, allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages]},
-                {id: myRole, allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages]},
+                {id: myRole.id, allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages]},
                 {id: interaction.guild.roles.everyone.id, deny: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages]}
 
              ],
